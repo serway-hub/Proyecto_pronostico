00de30db3232cc59f13683529b150c9dbf145c18
jorge luis <jorgeluis20033@hotmail.com>
2025-02-10 04:59:47 -0500

    actializacion V1

---
+++ convertidor.py
@@ -0,0 +1,7 @@
+from docx2pdf import convert
+
+# Convertir un archivo específico
+convert('programacion FEBRERO.docx', "programacion_febrero.pdf")
+
+# Convertir todos los archivos en un directorio
+# convert("ruta_del_directorio")
